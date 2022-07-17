c26506585211bcb6a40e7e19ad709b1d6160bcaf
MaiiNunes <maisa.t.nunes@gmail.com>
2022-07-17 18:45:05 -0300

    .

--- Aula 5-JavaScript/assets/js/scripts.js
+++ Aula 5-JavaScript/assets/js/scripts.js
@@ -1,1 +1,28 @@
-console.log("Hello world");
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function modifyText() {
+    if(currentNumber < 0){
+        document.getElementById("currentNumber").style.color = "red";
+    }
+    if(currentNumber >= 0){
+        document.getElementById("currentNumber").style.color = "green";
+    }
+}
+
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;    
+    
+}
+
+
+document.getElementById("negative").addEventListener('click', modifyText);
+document.getElementById("positive").addEventListener('click', modifyText);
